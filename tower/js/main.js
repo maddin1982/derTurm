f6ebd2f367f0560696d87b1e8a533c208486a323
M1chbeck <mr_b.k@web.de>
2014-11-12 00:21:33 +0100

    moved the transition button into each line

--- tower/js/main.js
+++ tower/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	init3DSceneOnElement($("#3DContainer"));
 	$("#addFrameBtn").on("click",framesManager.addFrame)
 	$("#saveSceneBtn").on("click",framesManager.saveDataToBackend)
-	$("#saveTransition").on("click",framesManager.saveDataToBackend);
+	$("#saveTransition").on("click",framesManager.setTransition);
 
 	// fenster anzahl auswählen
 	$("#2_fenster").on("click",{amount: 2},windowManager.setWindowAmount);
@@ -167,6 +167,10 @@ var framesManagerObj = function(framesContainer){
 	this.frameAnimationRunning=false;
 	var that=this;
 
+	function setTransition(){
+		//BUG is not called yet from the modal dialog :(
+		//$('#myModal').modal('hide')
+	}
 	//go to next Frame if there is one
 	function goToNextFrame(){
 		if(data.length>1){
@@ -265,8 +269,9 @@ var framesManagerObj = function(framesContainer){
 	this.renderFrames=function(){		
 		windowManager.updateData();
 		that.framesContainer.empty();
+		var frameDiv = null;
 		$.each(data,function(j,frame){
-			var frameDiv=document.createElement('div')
+			frameDiv=document.createElement('div')
 			$(frameDiv).attr("class","frame")
 			$(frameDiv).attr("frameId",j)
 			$.each(frame.windows,function(i,frameWindow){
@@ -280,7 +285,16 @@ var framesManagerObj = function(framesContainer){
 				$(windowDiv).on("click",that.selectFrame)
 				$(frameDiv).append(windowDiv)
 			})
+			var transitionA=document.createElement('a')
+			$(transitionA).attr("href","#myModal")
+			$(transitionA).attr("id","transitionBtn"+j)
+			$(transitionA).attr("role","button")
+			$(transitionA).attr("class","btn btn-xs btn-default transitionBtn")
+			$(transitionA).attr("data-toggle","modal")
+			$(transitionA).text("+")
+			$(frameDiv).append(transitionA)
 			that.framesContainer.append(frameDiv)
+
 		})
 	}
 }
