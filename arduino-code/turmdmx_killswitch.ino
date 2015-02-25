096ea4c8fa0cf0a4db4dd1a696fc846719aa2a0f
Nick <nick@bitfasching.de>
2015-02-26 00:33:03 +0100

    Added new DMX relay code for Arduino.
    - added a copy of the old code but with a kill switch instead of an idle animation
    - renamed the old code to clarify the difference

---
+++ arduino-code/turmdmx_killswitch.ino
@@ -0,0 +1,159 @@
+/**
+ * DMX-Controller für den Talking Tower
+ * – wartet mit Betrieb auf Verbindung zur seriellen Schnittstelle
+ * – lässt dem Host 20 Sekunden Zeit bis zum Timeout
+ * – schaltet bei Timeout Pin 3 auf HIGH und hängt sich unwiderruflich auf
+ *
+ * 01/2015, v2.0
+ */
+
+
+// DMX-Bibliothek
+#include <LeoDMX.class.h>
+
+// DMX-Instanz erzeugen
+LeoDMX DMX;
+
+// Versionsnummer
+#define VERSION "2.0"
+
+
+
+
+/* :: Einstellungen :: */
+
+
+// Host-Baudrate
+const unsigned long hostBaud = 115200;
+
+// Timeout zwischen DMX-Frames [ms]
+const unsigned short hostDataGap = 3;
+
+// auf Host warten?
+const bool waitForSerial = true;
+
+
+// Anzahl der verwendeten Kanäle (16x4)
+const int channelCount = 64;
+
+
+// Timeout für Empfang [ms]
+// (kommen für diese Zeit keine Daten an, läuft eine Notfallanimation)
+const unsigned lastUpdateTimeout = 20000;
+
+
+// Pin für Notschalter
+const int emergencyPin = 3;
+
+
+
+
+/* :: veränderliche Werte :: */
+
+
+// Zeitpunkt letzter empfangener Daten [ms]
+unsigned long lastUpdateTime;
+
+// Array für Kanaldaten
+char channels[ channelCount ] = {0};
+
+
+
+
+/* :: Arduino-Standardfunktionen :: */
+
+
+// Hostverbindung aufbauen
+void setup()
+{
+    // auf Schnittstelle warten
+    while ( waitForSerial and !Serial );
+    
+    // Verbindung aufbauen
+    Serial.begin( hostBaud );
+    Serial.setTimeout( hostDataGap );
+    
+    // Hallo sagen
+    Serial.println( "TurmDMX v" VERSION );
+    Serial.print( lastUpdateTimeout );
+    Serial.println( "ms until timeout." );
+    
+    // alle Strahler ausschalten
+    DMX.null();
+    
+    // Notfallschalter zurücksetzen
+    pinMode( emergencyPin, OUTPUT );
+    digitalWrite( emergencyPin, LOW );
+    
+    // Zeitpunkt für letzte Übertragung initialisieren
+    lastUpdateTime = millis();
+}
+
+
+// auf Kanaldaten warten und weiterleiten
+void loop()
+{
+    // versuchen, alle Kanalbytes zu lesen
+    short count = Serial.readBytes( channels, channelCount );
+    
+    // falls Daten gelesen wurden
+    if ( count > 0 )
+    {
+        // Daten weiterleiten
+        DMX.send( (unsigned char*) channels, count );
+        
+        // zurückmelden, wie viel gesendet wurde
+        Serial.print( "OK:" );
+        Serial.println( count );
+        
+        // merken, wann zuletzt etwas gesendet wurde
+        lastUpdateTime = millis();
+    }
+    
+    // nichts gelesen
+    else
+    {
+        // prüfen, ob ein Fehler vorliegt (lange nichts empfangen?)
+        if ( millis()-lastUpdateTime > lastUpdateTimeout )
+        {
+            // Bescheid geben
+            Serial.println( "Timeout!" );
+            
+            // Relais betätigen
+            digitalWrite( emergencyPin, HIGH );
+            
+            // Zeitpunkt des Fehlers merken
+            unsigned long errorTime = millis();
+            
+            // in Endlosschleife aufhängen
+            while (true)
+            {
+                // mit interner LED bei 1 Hz blinken
+                blinkLED( 13, 1000 );
+                
+                // anzeigen, wie lange der Fehler schon besteht
+                Serial.print( "Locked since timeout " );
+                Serial.print( (millis()-errorTime)/1000 );
+                Serial.println( " seconds ago." );
+            }
+        }
+    }
+}
+
+
+
+
+/* :: Helfer :: */
+
+
+// LED an gegebenem Pin für übergebene Zyklusdauer ein- & ausschalten
+void blinkLED( short pin, unsigned int cycleDuration )
+{
+    // für halben Zyklus einschalten
+    digitalWrite( pin, HIGH );
+    delay( cycleDuration / 2 );
+    
+    // für halben Zyklus ausschalten
+    digitalWrite( pin, LOW );
+    delay( cycleDuration / 2 );
+}
